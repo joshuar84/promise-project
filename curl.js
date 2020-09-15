2aa1a9c038fb7b518183d22359a4e7076b26d97a
joshuar84 <1984yearoftherat@gmail.com>
2020-09-15 10:29:27 -1000

    working on errors

--- curl.js
+++ curl.js
@@ -3,9 +3,9 @@ const fetch = require('node-fetch');
 const url = 'https://artii.herokuapp.com/make?text=curl++this';
 const invalidUrl = "https://artii.herokuFLAP.com/make?text=curl++this";
 
-fetch(url)
-    .then(res => res.text())
-    .then(txtRes => console.log(txtRes));
+// fetch(url)
+//     .then(res => res.text())
+//     .then(txtRes => console.log(txtRes));
     // .then((res) => res.json())
     // .then((result1) => {
     //     console.log(result1);
@@ -14,3 +14,15 @@ fetch(invalidUrl)
   .then((res) => res.text())
   .then((txtRes) => console.log(txtRes))
   .catch((err) => console.log(err))
+  .catch(err => {
+    // if error code is "ENOTFOUND" => host wasnt found
+    if (err.errno === 'ENOTFOUND') {
+      // creates a url object so we can extract certain data
+      // in this case all we want is the host, not the entire url
+      let host = new URL(invalidUrl).host;
+      return console.error('curl: (6) Could not resolve host: ', host)
+    } else {
+        console.log(err);
+    }
+  })
+  
