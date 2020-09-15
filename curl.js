e2a8a7343fc4d82eddb0e387fa155f635908eb02
brandon <bparsons17@liberty.edu>
2020-09-15 16:13:18 -0400

    phase 1 completed

--- curl.js
+++ curl.js
@@ -1,9 +1,16 @@
 const fetch = require('node-fetch');
 
 const url = 'https://artii.herokuapp.com/make?text=curl++this';
+const invalidUrl = "https://artii.herokuFLAP.com/make?text=curl++this";
 
 fetch(url)
-    .then((res) => res.json())
-    .then((result1) => {
-        console.log(result1);
-    })
+    .then(res => res.text())
+    .then(txtRes => console.log(txtRes));
+    // .then((res) => res.json())
+    // .then((result1) => {
+    //     console.log(result1);
+
+fetch(invalidUrl)
+  .then((res) => res.text())
+  .then((txtRes) => console.log(txtRes))
+  .catch((err) => console.log(err))
